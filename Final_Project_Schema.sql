804ccbed0f6cdadaf38bd8dce0d4fcb76d8f9564
mereilly <mereilly@loyola.edu>
2021-09-15 21:07:58 -0400

    copying files from this existing branch in Elodie's final repo

---
+++ Final_Project_Schema.sql
@@ -0,0 +1,28 @@
+-- Creating State & Lats/Longs table
+CREATE TABLE state_data_table (
+Date DATE,
+State TEXT PRIMARY KEY,
+Latitude INTEGER,
+Longitude INTEGER
+);
+
+-- Creating AQI table
+CREATE TABLE aqi_data_table (
+Date DATE,
+State TEXT PRIMARY KEY,
+AQI INTEGER
+);
+
+-- Creating pollutents table
+CREATE TABLE pollutents_data_table (
+Date DATE,
+State TEXT PRIMARY KEY,
+CO INTEGER,
+NO INTEGER,
+NO2 INTEGER,
+O3 INTEGER,
+SO2 INTEGER,
+PM2_5 INTEGER,
+PM10 INTEGER,
+NH3 INTEGER
+);
